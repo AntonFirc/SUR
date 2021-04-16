279706890f0afb2b574c09bb6f16be48203373a6
anton.firc <anton.firc@gmail.com>
2021-04-16 17:39:31 +0200

    Some training..?

--- speech_gaussian.py
+++ speech_gaussian.py
@@ -1,24 +1,25 @@
-from ikrlib import wav16khz2mfcc
+from multiprocessing.pool import ThreadPool
 from sklearn.mixture import GaussianMixture
 import librosa as l
 from pathlib import Path
 import numpy as np
+from tqdm import tqdm
 
 class_cnt = 0
-gaussian_cnt = 10
-
-gmm_arr = []
+gaussian_cnt = 2
 
 dev_path = Path('./dataset/dev')
 train_path = Path('./dataset/train')
 
-for speaker_dir in train_path.iterdir():
-    if str(speaker_dir).__contains__('.DS_Store'):
-        continue
+speakers = []
+gmm_arr = []
 
-    class_cnt += 1
 
+def process_speaker(speaker_dir):
     speaker_features = []
+    speaker_idx = int(str(speaker_dir).split('/').pop())
+
+    print('Processing speaker {0}'.format(speaker_idx))
 
     for speaker_file in speaker_dir.iterdir():
         if str(speaker_file).endswith('.wav'):
@@ -26,10 +27,38 @@ for speaker_dir in train_path.iterdir():
             n_fft = int(sr * 0.02)  # window length: 0.02 s
             hop_length = n_fft // 2  # usually one specifies the hop length as a fraction of the window length
             mfccs = l.feature.mfcc(x, sr=sr, n_mfcc=24, hop_length=hop_length, n_fft=n_fft)
-            # check the dimensions
             speaker_features.append(mfccs)
 
     features = np.concatenate(speaker_features, axis=1)
 
+    print('Training speaker {0}'.format(speaker_idx))
+
+    gmm = GaussianMixture(n_components=gaussian_cnt, max_iter=10).fit(features)
+    gmm_arr.insert(speaker_idx, gmm)
+
+    print('Speaker {0} finished!'.format(speaker_idx))
+
+for speaker_dir in train_path.iterdir():
+    if str(speaker_dir).__contains__('.DS_Store'):
+        continue
+
+    class_cnt += 1
+    speaker_features = []
+    speaker_idx = int(str(speaker_dir).split('/').pop())
+
+    speakers.append(speaker_dir)
+
+with ThreadPool(1) as pool:
+    list(
+        tqdm(
+            pool.imap(
+                process_speaker,
+                speakers
+            ),
+            'Process',
+            len(speakers),
+            unit="speakers"
+        )
+    )
 
 print(gmm_arr)
