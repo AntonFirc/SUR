c857d6d542d09b949096a8c4b9f957f5637b9674
Tomas Lapsansky <tomas.lapsansky.97@gmail.com>
2021-04-18 16:37:33 +0200

    PCA + LDA

--- photo_gaussian.py
+++ photo_gaussian.py
@@ -1,5 +1,8 @@
 import collections
+from glob import glob
 from multiprocessing.pool import ThreadPool
+
+from matplotlib import pyplot as plt
 
 from imread import imread
 from sklearn.mixture import GaussianMixture
@@ -8,10 +11,11 @@ from pathlib import Path
 import numpy as np
 from tqdm import tqdm
 
-import png
-
 class_cnt = 31
 gaussian_cnt = 10
+
+PCA_dim = 60
+LDA_dim = 20
 
 dev_path = Path('./dataset/dev')
 train_path = Path('./dataset/train')
@@ -20,19 +24,68 @@ participants = []
 gmm_arr = {}
 
 
-def train_participant(photo_dir):
-    participant_idx = int(str(photo_dir).split('/').pop())
-    photo_features = png.png2fea(photo_dir)
+def png2fea(dir_name):
+    """
+    Loads all *.png images from directory dir_name into a dictionary. Keys are the file names
+    and values and 2D numpy arrays with corresponding grayscale images
+    """
+    features = []
+    for f in glob(str(dir_name) + '/*.png'):
+        features.append(calc_feature(f))
+    return features
 
-    features = np.concatenate(photo_features, axis=0)
 
-    gmm = GaussianMixture(n_components=gaussian_cnt, max_iter=200).fit(features)
-    gmm_arr[participant_idx] = gmm
+def calc_feature(file):
+    return imread(file, True).astype(np.float64)
+
+
+def train_participants():
+    for i in range(class_cnt):
+        participant_dir = train_path.joinpath(str(i + 1))
+        participants.append(participant_dir)
+
+    # feature_arr = {}
+    # for participant_dir in participants:
+    #     participant_idx = int(str(participant_dir).split('/').pop())
+    #     photo_features = png2fea(participant_dir)
+    #     feature_arr[participant_idx] = photo_features
+
+    feature_list = []
+    for participant_dir in participants:
+        participant_idx = int(str(participant_dir).split('/').pop())
+        photo_features = png2fea(participant_dir)
+        feature_list.insert(participant_idx, photo_features)
+
+    # features = np.concatenate(photo_features, axis=0)
+
+    reshaped_features = np.array(feature_list).reshape((6*31, 6400))
+    mean_face = np.mean(reshaped_features, axis=0)
+    features = reshaped_features - mean_face
+
+    # Display mean face
+    # plt.imshow(mean_face.reshape((80, 80)), interpolation='nearest')
+    # plt.show()
+
+    u, s, _ = np.linalg.svd(features)
+    reshaped_u = u[:, :PCA_dim]
+    train_pca = reshaped_u.T @ features
+
+    # Display PCAs
+    # for pca in train_pca:
+    #     plt.imshow(pca.reshape((80, 80)), interpolation='nearest')
+    #     plt.show()
+
+    # gmm = GaussianMixture(n_components=gaussian_cnt, max_iter=200).fit(features)
+    # # gmm_arr[participant_idx] = gmm
+
+    # LDA
+    wc_cov = np.zeros((PCA_dim, PCA_dim))
+    for i in range(class_cnt):
+        wc_cov += wc_cov + np.cov(train_pca())
 
 
 def eval_participant(recording_path):
     scores = []
-    # recording_path = './dataset/train/28/m429_01_f13_i0_0.png'
 
     f = recording_path
     mfccs = imread(f, True).astype(np.float64)
@@ -66,24 +119,9 @@ def evaluate_model():
     print('Total accuracy: {0}%'.format(model_acc * 100))
 
 
-for i in range(class_cnt):
-    participant_dir = train_path.joinpath(str(i + 1))
-    participants.append(participant_dir)
 
-with ThreadPool(1) as pool:
-    list(
-        tqdm(
-            pool.imap(
-                train_participant,
-                participants
-            ),
-            'Process',
-            len(participants),
-            unit="participants"
-        )
-    )
+train_participants()
 
 gmm_ord = collections.OrderedDict(sorted(gmm_arr.items()))
 
 evaluate_model()
-# print(gmm_arr)
